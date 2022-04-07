278b134fb075e9af569947011190a1778666dff7
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-07 23:35:56 +0200

    test(cli): add cli runner

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,3 +6,9 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 #==============================================================================
+
+import utilatest
+
+import headlines
+
+run, fail = utilatest.create_cli_runner(headlines)
