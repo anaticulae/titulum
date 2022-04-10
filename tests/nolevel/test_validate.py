ba678a508a7a495b752d7fe3c57b01a9f685b36d
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-10 16:20:42 +0200

    test(nolevel): validate docu027

--- tests/nolevel/test_validate.py
+++ tests/nolevel/test_validate.py
@@ -30,13 +30,14 @@ ARCHIVE = utila.join(headlines.ROOT, 'tests/nolevel/expected', exist=True)
     pytest.param(power.BACHELOR086_PDF, id='bachelor086'),
     pytest.param(power.BACHELOR090_PDF, id='bachelor090'),
     pytest.param(power.BACHELOR105_PDF, id='bachelor105'),
+    pytest.param(power.DISS172_PDF, id='diss172'),
+    pytest.param(power.DOCU027_PDF, id='docu027'),
     pytest.param(power.MASTER031_PDF, id='master031'),
     pytest.param(power.MASTER063_PDF, id='master063'),
     pytest.param(power.MASTER072_PDF, id='master072'),
     pytest.param(power.MASTER083_PDF, id='master083'),
     pytest.param(power.MASTER089_PDF, id='master089'),
     pytest.param(power.MASTER098_PDF, id='master098'),
-    pytest.param(power.DISS172_PDF, id='diss172'),
 ])
 def test_nolevel_validate(source, testdir, monkeypatch):
     utilatest.fixture_requires(source)
