aa286b0223abababf28be20241cafa7c8b421e42
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-10 21:38:15 +0200

    test(cluster): validate a lot of documents

--- tests/cluster/test_validate.py
+++ tests/cluster/test_validate.py
@@ -20,6 +20,16 @@ ARCHIVE = utila.join(headlines.ROOT, 'tests/cluster/expected', exist=True)
 
 @pytest.mark.parametrize('source', [
     pytest.param(power.BACHELOR028_PDF, id='bachelor028'),
+    pytest.param(power.BACHELOR032A_PDF, id='bachelor032a'),
+    pytest.param(power.BACHELOR032_PDF, id='bachelor032'),
+    pytest.param(power.BACHELOR036_PDF, id='bachelor036'),
+    pytest.param(power.BACHELOR039_PDF, id='bachelor039'),
+    pytest.param(power.BACHELOR041A_PDF, id='bachelor041a'),
+    pytest.param(power.BACHELOR067_PDF, id='bachelor067'),
+    pytest.param(power.BACHELOR078_PDF, id='bachelor078'),
+    pytest.param(power.BACHELOR086_PDF, id='bachelor086'),
+    pytest.param(power.BACHELOR090_PDF, id='bachelor090'),
+    pytest.param(power.BACHELOR105_PDF, id='bachelor105'),
 ])
 def test_cluster_validate(source, testdir, monkeypatch):
     utilatest.fixture_requires(source)
