1ac3d6d29123637a869553f35e57b3551c62c0ee
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-10 20:12:05 +0200

    feat(cluster): add method to validate cluster

---
+++ headlines/cluster/validate.py
@@ -0,0 +1,121 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021-2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import statistics
+
+import elements
+import utila
+
+
+def valid_headline_clusters(
+    clusters,
+    cluster_size_min: int = 5,
+    cluster_rate_min: float = 0.3,
+    cluster_headline_median_length_min: int = 10,
+    x0_max_diff: float = 100.0,
+    whitespace_rate_max: float = 0.2,
+):
+    collected = []
+    delete = []
+    for cluster in clusters:
+        cluster = clean_cluster(cluster, x0_max_diff=x0_max_diff)
+        if len(cluster) <= cluster_size_min:
+            continue
+        rate, median = headline_rate(cluster)
+        if rate < cluster_rate_min:
+            continue
+        if median < cluster_headline_median_length_min:
+            continue
+        if noheadline_cluster(cluster):
+            continue
+        if whitespace_rate(cluster) > whitespace_rate_max:
+            continue
+        collected.append(cluster)
+        delete.append(cluster)
+    flat = utila.flatten(collected)
+    return flat, delete
+
+
+PAGE_HEIGHT = 841.89
+HEADER_HEIGHT = 70.0
+FOOTER_HEIGHT = 120.0
+
+
+def clean_cluster(
+    cluster,
+    x0_max_diff: float = 15.0,
+    ymin: float = HEADER_HEIGHT,
+    ymax: float = PAGE_HEIGHT - FOOTER_HEIGHT,
+):
+    # TODO: ACCEPT RIGHT PADDED TEXT
+    # skip too right items
+    valid = [
+        item for item in cluster
+        if 35.0 <= item[0].bounding.x0 < (75.0 + x0_max_diff)
+    ]
+    # skip `Kapitel 1`-pattern
+    valid = [
+        item for item in valid if not elements.noheadline_pattern(item[0].text)
+    ]
+    # TODO: THIS REMOVES ALL HEADLINES WITH NUMBER AT THE END
+    # remove:
+    # 5.1. ZUSAMMENFASSUNG DER ERGEBNISSE UND SCHLUSSFOLGERUNGEN   198
+    # 5.2. KÜNFTIGE HERAUSFORDERUNGEN       203
+    valid = [
+        item for item in valid
+        if not utila.isnumber(item[0].text.split(' ')[-1])
+    ]
+    # Lebenslauf .............................. xx
+    valid = [item for item in valid if item[0].text.count('.') < 6]
+    # in bounds
+    valid = [
+        item for item in valid
+        if ymin <= item[0].bounding.y0 <= item[0].bounding.y1 <= ymax
+    ]
+    return valid
+
+
+def headline_rate(cluster):
+    # TODO: MOVE TO ELEMENTS?
+    median = statistics.median([len(item[0].text) for item in cluster])
+    headlines = [
+        item for item in cluster if elements.isheadline(
+            item[0].text,
+            strict=False,
+        )
+    ]
+    return len(headlines) / len(cluster), median
+
+
+def whitespace_rate(cluster) -> float:
+    charcount = 0
+    whitespaces = 0
+    for item in cluster:
+        charcount += len(item[0].text)
+        whitespaces += item[0].text.count(' ')
+    if not charcount:
+        return 0.0
+    return whitespaces / charcount
+
+
+def noheadline_cluster(cluster, pagerate_max: float = 0.5):
+    """\
+     2. Klassifikation 5
+     3. Steuerung 13
+     4. Anwendung 17
+    """
+    if len(cluster) < 4:
+        return False
+    with_pageending = [
+        item for item in cluster if utila.isnumber(item[0].text.split(' ')[-1])
+    ]
+    pagerate = len(with_pageending) / len(cluster)
+    if pagerate > pagerate_max:
+        return True
+    return False
