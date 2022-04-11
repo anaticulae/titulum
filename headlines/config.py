1fe8b036574e97eaeb2d17001d3ae97c6c326e28
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-11 21:18:43 +0200

    feat(config): add table with minimal headline count

---
+++ headlines/config.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import configo
+
+# minimal headline count for a valid extraction, depending on document
+# length.
+HEADLINE_COUNT_MIN = configo.HolyTable(items=(
+    (0, 6),
+    (20, 6),
+    (30, 8),
+    (100, 12),
+    (150, 15),
+    (200, 15),
+    (300, 20),
+))
