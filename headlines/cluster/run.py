4ef9fe053014736211382200a669a06d35da1961
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-22 17:45:24 +0200

    feat(cluster): skip hidden items

--- headlines/cluster/run.py
+++ headlines/cluster/run.py
@@ -59,6 +59,8 @@ def extract_headlines(
     )
     # merge multiple headline
     flat = headlines.cluster.merge.merge_headline(flat)
+    # skip hidden items
+    flat = [item for item in flat if item.visible]
     headline_count_min = headlines.config.HEADLINE_COUNT_MIN(document_length)
     if len(flat) < headline_count_min:
         utila.debug(f'cluster: too few headlines {len(flat)}, require at '
